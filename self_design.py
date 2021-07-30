f3d1511b0aac2781e53ac194d21c987d841f57c2
lijia <lijia0732@sina.com>
2021-07-30 09:41:17 +0800

    解决中文乱码的问题

--- self_design.py
+++ self_design.py
@@ -6,7 +6,9 @@ import globals
 @globals.task(type = "my_task_list_latest_posts", description =\
     "输出最近的博客")
 def get_latest_posts(*kargs, **kwargs):
-    xml = requests.get("https://nicelee.top/feed.xml").text
+    result = requests.get("https://nicelee.top/feed.xml")
+    result.encoding = "UTF-8"
+    xml = result.text
     it = re.finditer("<item>.*?<title>(.*?)</title>.*?<pubDateFormat>(.*?)</pubDateFormat>.*?<link>(.*?)</link>.*?</item>" , xml, flags = re.DOTALL)
     posts = [ (match.group(1), match.group(3), match.group(2)) for match in it]
     posts = posts[:4]
